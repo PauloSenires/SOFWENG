2853475c8380626b94810cd52e1f7e91385ec40a
Mave <Mave@DESKTOP-G7K0Q4O>
2018-11-02 02:07:27 +0800

    initial push

---
+++ SOFWENG/src/sofweng/Faculty.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sofweng;
+
+/**
+ *
+ * @author Mave
+ */
+public class Faculty extends User{
+    public String[] subjectList;
+    public void inputGrade(){
+        
+    }
+    public void inputSOGrade(){
+    }
+    public void editGrade(){
+        
+    }
+    public void editSOGrade(){
+        
+    }
+    public void viewClassList(){
+    }
+    public void uploadSOEvidence(){
+        
+    }
+}
---
+++ SOFWENG/src/sofweng/User.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sofweng;
+
+/**
+ *
+ * @author Mave
+ */
+public class User {
+    public User(){
+        
+    }
+    public String idNumber="";
+    private String passWord="";
+    public String emailAddress="";
+    public String name = "";
+    public String department = "";
+    public String contactNumber = "";
+    public String securityPIN = "";
+    public String accessLevel ="";
+    
+    public void LogIn(){
+        
+    }
+    public void Register(){
+        
+    }
+    public String getPass(){
+        return passWord;
+    }
+}
