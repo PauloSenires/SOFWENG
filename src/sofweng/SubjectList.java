88cb3a1ad38801a1c48510e40ce4c457f0aa7b59
iwcnrlee1 <iwcnrlee1@iwcnrlee1-PC.local>
2018-11-20 15:12:44 +0800

    Added export notification

--- src/sofweng/SubjectList.java
+++ src/sofweng/SubjectList.java
@@ -540,6 +540,11 @@ public final class SubjectList extends javax.swing.JFrame {
         workbook.write();
         workbook.close();
         counter=0;
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new ExcelNotification().setVisible(true);
+            }
+        });
     }
     
     public void write(ArrayList<String> text) throws WriteException{
@@ -555,7 +560,7 @@ public final class SubjectList extends javax.swing.JFrame {
     /**
      * @param args the command line arguments
      */
-    public static void main(String args[]) {
+    public static void main(String name) {
         /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
@@ -583,7 +588,7 @@ public final class SubjectList extends javax.swing.JFrame {
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new SubjectList("").setVisible(true);
+                new SubjectList(name).setVisible(true);
             }
         });
     }
